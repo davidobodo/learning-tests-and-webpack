46a0b5ae093be63bcfdea05c87882b44feb3bf8d
David Obodo <obododavid5@gmail.com>
2020-04-24 07:08:11 +0100

    dev server compiling with babel

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            Hello this is my app built with webpack from scratch
+        </div>
+    )
+}
+
+export default App;
