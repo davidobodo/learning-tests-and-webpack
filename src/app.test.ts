9ea95e87d63efc8396b4320992d85955f7a27ace
David Obodo <obododavid5@gmail.com>
2020-05-02 03:01:33 +0100

    install stuffs instructed by docs

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,17 +1,16 @@
 import { unmountComponentAtNode } from "react-dom";
-import { tsExternalModuleReference } from "@babel/types";
 
 let container = null;
 
 beforeEach(() => {
-    // setup a DOM element as a render target
     container = document.createElement("div");
     document.body.appendChild(container);
 });
 
 afterEach(() => {
-    // cleanup on exiting
     unmountComponentAtNode(container);
     container.remove();
     container = null;
 });
+
+it('renders correctly')
