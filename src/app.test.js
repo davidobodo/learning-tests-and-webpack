beb0582205c728999c8027e91b524b64c26b6c02
David Obodo <obododavid5@gmail.com>
2020-05-12 10:17:28 +0100

    succesfully enabling button when all validation criteria are met

--- src/app.test.js
+++ src/app.test.js
@@ -29,7 +29,7 @@ it('should display a button with text submit', () => {
     expect(getByTestId('submit-button')).toHaveTextContent('Submit')
 })
 
-it('should be disabled', () => {
+it('should be disabled initially', () => {
     const { getByTestId } = render(<App />);
     expect(getByTestId('submit-button')).toBeDisabled();
 })
@@ -47,35 +47,34 @@ it('should render a snapshot of input field with a class of error', () => {
     expect(container).toMatchSnapshot();
 })
 
+it('Button should be enabled when all validations are met', () => {
+    const { getByTestId, getByLabelText } = render(<App />);
+    const fullname = getByLabelText('Obodo David');
+    fireEvent.change(fullname, { target: { value: 'Obodo Geek' } })
+
+    const email = getByLabelText('obododavid@gmail.com');
+    fireEvent.change(email, { target: { value: 'phitgeek@gmail.com' } })
+
+    const phone = getByLabelText('080XXXXXXXX');
+    fireEvent.change(phone, { target: { value: '08105546642' } })
+
+    const password = getByLabelText('Password');
+    fireEvent.change(password, { target: { value: 'Aa1@aaa' } })
+
+    const cPassword = getByLabelText('Confirm Password');
+    fireEvent.change(cPassword, { target: { value: 'Aa1@aaa' } })
+
+    const cNumber = getByLabelText('XXXX XXXX XXXX XXXX');
+    fireEvent.change(cNumber, { target: { value: '5627463526372649' } })
+
+    const eDate = getByLabelText('mm/yy');
+    fireEvent.change(eDate, { target: { value: '2390' } })
+
+    const pin = getByLabelText('****');
+    fireEvent.change(pin, { target: { value: '0000' } })
+
+    expect(getByTestId('submit-button')).toBeEnabled();
+
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// test('render correctly inputs', () => {
-//     let component = renderer.create(<App />).toJSON();
-//     expect(component).toMatchSnapshot();
-// })
-
-
-//enzyme
-// it('should change state after change in input value', () => {
-//     let component;
-//     let newValue = 'hello';
-//     const wrapper = mount(<Auth />);
-
-//     expect(wrapper.find('input[name="fullName"]').prop('value')).toEqual('david');
-//     wrapper.find('input[name="fullName"]').simulate('change', { target: { name: 'fullName', value: newValue } });
-//     expect(wrapper.find('input[name="fullName"]').prop('value')).toEqual(newValue)
-// })
